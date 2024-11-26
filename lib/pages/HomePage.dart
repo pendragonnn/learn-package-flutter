75da9c76d1b1568d927dd53cde1655a40710bcd4
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2024-11-26 13:32:38 +0700

    add: intl

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:faker/faker.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatelessWidget {
   var faker = new Faker();
@@ -14,15 +15,19 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: ListView.builder(
-        itemCount: 50,
+        itemCount: 5,
         itemBuilder: (context, index) => ListTile(
           leading: CircleAvatar(
             backgroundColor: Colors.grey[300],
             backgroundImage:
                 NetworkImage("https://picsum.photos/id/${879 + index}/200/300"),
           ),
+          // faker implementation
           title: Text(faker.person.name()),
-          subtitle: Text(faker.internet.email()),
+
+          // intl implementation
+          subtitle:
+              Text("${DateFormat.yMMMMd().add_jms().format(DateTime.now())}"),
         ),
       ),
     );
